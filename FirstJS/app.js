37f2aacccd0d692077b4c0e103adf794663ece2c
chomihye <devmihye@gmail.com>
2022-06-08 00:15:10 +0900

    create app.js

---
+++ FirstJS/app.js
@@ -0,0 +1,34 @@
+const a = 5;
+const b = 2;
+
+const myName = 'kado';
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+
+2.array
+const daysOfweek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+
+//Get itme from Array
+console.log(daysOfweek);
+
+//add one more day to the array
+daysOfweek.push('sun');
+console.log(daysOfweek);
+
+const toBuy = ['poatato', 'tomato', 'pizza'];
+toBuy.push("cola")
+
+//3.object
+const player = {
+  name: 'kado',
+  points: 10,
+  fat: true,
+};
+
+console.log(player);
+console.log(player['name']);
+console.log(player.name);
+player.name;
+player.lastname = 'cat';
